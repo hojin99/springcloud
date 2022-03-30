923faef747962a4ad06fa0d83c3a94e761703e28
조호진 <hojin99@naver.com>
2022-03-30 20:42:54 +0900

    User Service 추가

--- ec-user-service/src/main/java/com/inno/sc/ecuser/controller/UserServiceController.java
+++ ec-user-service/src/main/java/com/inno/sc/ecuser/controller/UserServiceController.java
@@ -4,11 +4,14 @@ import com.inno.sc.ecuser.dto.UserDto;
 import com.inno.sc.ecuser.service.UserService;
 import com.inno.sc.ecuser.vo.Greeting;
 import com.inno.sc.ecuser.vo.RequestUser;
+import com.inno.sc.ecuser.vo.ResponseUser;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -36,14 +39,16 @@ public class UserServiceController {
     }
 
     @PostMapping("/users")
-    public String createUser(@RequestBody RequestUser user) {
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user) {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserDto userDto = mapper.map(user, UserDto.class);
         userService.createUser(userDto);
 
-        return "Create user method is called";
+        ResponseUser reponseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(reponseUser); // 201
     }
 
     @GetMapping("/users")
